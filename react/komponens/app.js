3e432ba81de47c87836f8d571a898e01c1968715
Gellértfy "Huto-GITANES" Tamás <kisfifi2006@gmail.com>
2024-12-18 12:09:20 +0100

    12.11
    app.js

---
+++ react/komponens/app.js
@@ -0,0 +1,39 @@
+function App(){
+    
+    return React.createElement( //létre hozunk egy divet
+        "div",
+        {
+            className:"border" //meghívunk egy bootstrapes osztályt
+        },
+        "App",
+        React.createElement(BoxComponent),
+        React.createElement(BoxComponent),
+        React.createElement(BoxComponent)
+
+ 
+    );
+}
+ 
+//BoxComponens létrehozása a megadott stíluselemekkel
+function BoxComponent(props){
+    return React.createElement( //létre hozunk egy divet
+        "div",
+ 
+        {
+ 
+            style:{
+                width:"200px",
+                height:"200px",
+                backgroundColor:"red",
+ 
+            },
+            className:"p-2 m-5 rounded", //meghívunk egy bootstrapes osztályt
+        },
+    );
+}
+//az app componens a render függvénnyel meghívja az app-containert
+ReactDOM.render (
+    React.createElement(App),
+    document.getElementById("app-container")
+ 
+);
