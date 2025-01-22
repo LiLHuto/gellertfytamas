b48a9f6f8b3b7719029c1907b650fdab03fb2c6a
Gellértfy "Huto-GITANES" Tamás <kisfifi2006@gmail.com>
2025-01-22 10:19:25 +0100

    orai munka

---
+++ backend/gyakorlófeladat/backend/server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const mysql = require ("mysql");
+const bodyParser = require("body-parser");
+app.use(bodyParser.json());
+app.use(cors());
+
+const db = mysql.createConnection({
+    user: "root",
+    host: "127.0.0.1",
+    password: "",
+    database:"kozutak",
+})
